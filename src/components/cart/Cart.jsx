5d6f2ae462a3a6af4d499bc7f40367eb28ef26fd
Arman Hossain <armanmahim00@gmail.com>
2024-11-03 17:41:29 +0600

    new add

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import Bottle from "../bottle/Bottle";
+
+import PropTypes from 'prop-types'
 import './cart.css'
 
 const cart = ({cart}) => {
@@ -7,11 +8,16 @@ const cart = ({cart}) => {
               <h4>Cart: {cart.length}</h4>
               <div className="cart-container">
                 {
-                    cart.map(bottle => <img src={bottle.img}></img>)
+                    cart.map(bottle => <img key={bottle.id} src={bottle.img}></img>)
                 }
               </div>
         </div>
     );
 };
 
+
+cart.PropTypes={
+
+    cart:PropTypes.array.isRequired
+}
 export default cart;
